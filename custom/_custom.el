29854215a763a5948d7165b49eeb61553a422ae4
Arthur Chang <arthurc.iphone@gmail.com>
2016-12-13 16:20:45 +0800

    change font

--- custom/_custom.el
+++ custom/_custom.el
@@ -14,8 +14,8 @@
  )
 
 ;; 設定文字
-;;(set-default-font "-WenQ-文泉驛等寬正黑-normal-normal-normal-*-30-*-*-*-*-0-iso10646-1")
-(set-default-font "Inconsolata 24")
+(set-default-font "-WenQ-文泉驛等寬正黑-normal-normal-normal-*-30-*-*-*-*-0-iso10646-1")
+;;(set-default-font "Inconsolata 24")
 ;;(set-default-font "Consolas 22")
 
 ;; 設定行距
