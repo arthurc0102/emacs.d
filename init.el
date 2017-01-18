b64c369fc533f4477733619d12592941e983a94b
Arthur Chang <arthurc.iphone@gmail.com>
2017-01-18 11:04:49 +0800

    add time display

--- init.el
+++ init.el
@@ -58,3 +58,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Display time
+(defface egoge-display-time
+  '((((type x w32 mac))
+     ;; #060525 is the background colour of my default face.
+     (:foreground "#FFFFFF" :inherit bold))
+    (((type tty))
+     (:foreground "write")))
+  "Face used to display the time in the mode line.")
+
+;; This causes the current time in the mode line to be displayed in
+;; `egoge-display-time-face' to make it stand out visually.
+(setq display-time-string-forms
+      '((propertize (concat " " 24-hours ":" minutes " ")
+ 		    'face 'egoge-display-time)))
+
+(display-time-mode 1)
