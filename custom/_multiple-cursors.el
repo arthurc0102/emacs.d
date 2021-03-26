c5f0bbba2130b4ff6fc857c25dc7b10e4b854a74
Arthur Chang <arthurc0102@gmail.com>
2021-03-26 15:38:40 +0800

    Update multiple cursors keys

--- custom/_multiple-cursors.el
+++ custom/_multiple-cursors.el
@@ -1,6 +1,11 @@
-(global-set-key (kbd "C-s-c C-s-c") 'mc/edit-lines)
+(require 'multiple-cursors)
+
+(global-set-key (kbd "C-c e") 'mc/edit-beginnings-of-lines)
+(global-set-key (kbd "C-c a") 'mc/mark-all-like-this)
+
 (global-set-key (kbd "M-n") 'mc/mark-next-like-this)
 (global-set-key (kbd "M-p") 'mc/mark-previous-like-this)
-(global-set-key (kbd "M-N") 'mc/mark-next-like-this)
-(global-set-key (kbd "M-P") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "M-N") 'mc/skip-to-next-like-this)
+(global-set-key (kbd "M-P") 'mc/skip-to-previous-like-this)
+(global-set-key (kbd "C-c n") 'mc/unmark-next-like-this)
+(global-set-key (kbd "C-c p") 'mc/unmark-previous-like-this)
