6c3a357d4d82694fd220ca80d3fcff87df956543
Arthur Chang <arthurc0102@gmail.com>
2021-03-26 14:07:33 +0800

    Remove not needed config

--- custom/_autopair.el
+++ custom/_autopair.el
@@ -1,9 +1,1 @@
 (electric-pair-mode 1)
-
-(add-hook 'python-mode-hook
-          (lambda ()
-            (define-key python-mode-map "\"" 'electric-pair)
-            (define-key python-mode-map "\'" 'electric-pair)
-            (define-key python-mode-map "(" 'electric-pair)
-            (define-key python-mode-map "[" 'electric-pair)
-            (define-key python-mode-map "{" 'electric-pair)))
