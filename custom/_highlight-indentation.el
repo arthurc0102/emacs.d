3ce1bb1221c875461bfb35aa1098f45895d7f794
Arthur Chang <arthurc0102@gmail.com>
2021-07-13 17:42:01 +0800

    Update indentatoin color

--- custom/_highlight-indentation.el
+++ custom/_highlight-indentation.el
@@ -9,3 +9,6 @@
           (lambda () (highlight-indentation-current-column-mode)))
 (add-hook 'yaml-mode-hook
           (lambda () (highlight-indentation-set-offset 2)))
+
+(set-face-background 'highlight-indentation-face "#434445")
+(set-face-background 'highlight-indentation-current-column-face "#5b5c5e")
